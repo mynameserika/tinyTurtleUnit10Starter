bcb952333a953585e14cf7a308df9eef6643ea95
Mynameserika <erika.cabrera15@aol.com>
2017-03-08 22:11:07 +0000

    all three of my shapes

--- script.js
+++ script.js
@@ -11,34 +11,56 @@ forward(size);
 right(90);
 forward(size);
 }
-function shape(size){
-forward(size);
-right(50);
-forward(size);
-right(50);
-forward(size);
-right(50);
-forward(size);
-right(50);
-forward(size);
-}
 
 function triangle(size){
 forward(size);
-right(80);
+right(120);
 forward(size); 
-right(80);
+right(120);
 forward(size);
-right(80);
-forward(size);
-right(80);
-forward:(size);
 }
+
+function hexgon(size){
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+}
+
+function shape(type, size){
+    if(type === "triangle"){
+        triangle(size);
+    } else if (type==="square") {
+        square(size);
+    }else if (type==="hexgon"){
+        hexgon(size);
+    
+    }else{
+        alert("invaild answer");
+    }
+}
+
+
 // Type your function call below
-square(15);
-square(80);
-shape(85);
-shape(63);
-triangle(90);
-triangle(100);
+// square(15);
+// square(80);
+
+// triangle(90);
+// triangle(100);
+
+shape('triangle',99);
+shape('square',50);
+shape('hexgon',70);
+//shape('cirlce',40)
+
 stamp();
