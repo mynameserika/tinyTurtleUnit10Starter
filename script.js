fe5aef5031d22ce9ab1e3eeef1f978206e55f1f3
Mynameserika <erika.cabrera15@aol.com>
2017-03-08 21:05:03 +0000

    my sahpes

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(size){
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
+}
+function shape(size){
+forward(size);
+right(50);
+forward(size);
+right(50);
+forward(size);
+right(50);
+forward(size);
+right(50);
+forward(size);
 }
 
+function triangle(size){
+forward(size);
+right(80);
+forward(size); 
+right(80);
+forward(size);
+right(80);
+forward(size);
+right(80);
+forward:(size);
+}
 // Type your function call below
-
-
+square(15);
+square(80);
+shape(85);
+shape(63);
+triangle(90);
+triangle(100);
 stamp();
